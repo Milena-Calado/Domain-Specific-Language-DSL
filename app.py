4d8372b4c6c050a56ccb03d7aea34bbb39353ba6
Milena-Calado <mcsa2@cin.ufpe.br>
2023-11-23 13:05:36 -0300

    Update servidor

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import subprocess
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,29 +9,32 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/run_code', methods=['POST'])
+@app.route('/run_code', methods=['GET'])
 def run_code():
     try:
-        # Receba o código gerado do Blockly
-        generated_code = request
-        print(generated_code)
+         # Receba o código gerado do Blockly
+        generated_code = str(request.data, 'utf-8')
 
       # Salve o código em um arquivo (opcional)
         with open('generated_code.py', 'w') as file:
             file.write(generated_code)
-
    
         # Execute o código
-        ret = subprocess.run(['python', 'generated_code.py'], shell=True)
+        ret = subprocess.run(['python', 'generated_code.py'], check=True, capture_output=True)
         print(ret)
-        result = "Código executado com sucesso!"
+        result = {
+            'stdout': ret.stdout.decode('utf-8'),
+            'stderr': ret.stderr.decode('utf-8'),
+            'returncode': ret.returncode
+
+        }
 
     except Exception as e:
         # Trate exceções
         result = f"Erro ao executar o código: {e}"
 
     # Pode retornar resultados para o frontend se necessário
-    return result
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)

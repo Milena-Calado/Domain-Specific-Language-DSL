f7ab69a9b5a158b72b133deab32f32a2f3fb60c4
Milena-Calado <mcsa2@cin.ufpe.br>
2023-11-24 14:16:37 -0300

    Validation DSL for the robot

---
+++ static/src/generated_code.py
@@ -0,0 +1,30 @@
+from robot import Robot
+
+obj = Robot()
+
+import random
+
+default = None
+
+"""Describe this function...
+"""
+def do_something():
+  global default
+  pass
+
+
+if False:
+  pass
+
+for count in range(10):
+  pass
+
+1 + 1
+
+[]
+
+print('abc')
+
+'#%06x' % random.randint(0, 2**24 - 1)
+
+default = default

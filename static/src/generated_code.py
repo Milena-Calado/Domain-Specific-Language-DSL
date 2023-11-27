88dd9d0f67b87fe1ddaf282bf45c0534ee2e822b
Milena-Calado <mcsa2@cin.ufpe.br>
2023-11-27 16:27:26 -0300

    Update in the function index.html

--- static/src/generated_code.py
+++ static/src/generated_code.py
@@ -2,19 +2,4 @@ from robot import Robot
 
 obj = Robot()
 
-obj.connect('192.168.2.10')
-if True:
-  obj.move_to_home()
-for count in range(2):
-  obj.move_cartesiann([0, 0, 0, 0, 0, 0])
-  obj.close_tool()
-  obj.move_cartesiann([0, 0, 0, 0, 0, 0])
-  obj.move_cartesiann([0, 0, 0, 0, 0, 0])
-  obj.open_tool(0.60)
-obj.move_cartesiann([0, 0, 0, 0, 0, 0])
-obj.close_tool()
-obj.move_cartesiann([0, 0, 0, 0, 0, 0])
-obj.move_cartesiann([0, 0, 0, 0, 0, 0])
-obj.open_tool(0.60)
 obj.move_to_home()
-obj.disconnect()
